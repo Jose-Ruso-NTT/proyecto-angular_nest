91b5227f5824d28b4e09a7c94ae1627ea36ac8fd
Jose Francisco Ruso Alvarez <jrusoalv@emeal.nttdata.com>
2024-08-27 13:04:24 +0200

    front: user service

---
+++ frontend/src/services/api.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  #http = inject(HttpClient);
+
+  #apiUrl = environment.apiUrl;
+
+  getData() {
+    return this.#http.get(`${this.#apiUrl}/users`);
+  }
+}
